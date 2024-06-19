798c8d598dc4f038dcd41df6c0ee71dd7ee7ccc3
jesse davis <jdavis.prsvr@gmail.comgit config --global user.name jesse>
2024-06-19 17:02:57 -0500

    hope this is right

---
+++ JS/assignments/week06/an-object-lesson/main.js
@@ -0,0 +1,84 @@
+const myPersevereCodeObj = {
+  wait: 'I do not object'
+};
+
+// Part 1 - Adding and changing properties in plain JS.
+// Here, we'll be changing and adding properties BELOW the creation of the object.
+// You won't change lines 1-3 above, but instead set and change the properties for each step below by writing NEW code underneath each comment.
+
+// Below, set the value of property `wait` to be "I object".
+myPersevereCodeObj.wait = "I object"
+console.log(myPersevereCodeObj.wait)
+// console.log(property)
+// Add the new property `isAnExcellentObject` and set it to `true`.
+// const isAnExcellentObject = true;
+// console.log(isAnExcellentObject)
+myPersevereCodeObj.isAnExcellentObject = true;
+console.log(myPersevereCodeObj.isAnExcellentObject)
+// Add the property `term` and set it to 1.
+// const term = 1;
+// console.log(term)
+myPersevereCodeObj.term = 1;
+console.log(myPersevereCodeObj)
+// Now add 1 to the value of your object's `term` property and put that new value back in your object's `term`.
+// let newObjectTerm = isAnExcellentObject + 1;
+// console.log(newObjectTerm, isAnExcellentObject
+myPersevereCodeObj.term = myPersevereCodeObj.term +1;
+console.log(myPersevereCodeObj)
+// Now add a new property called `currentScore` and set it to 3.
+myPersevereCodeObj.currentScore = 3;
+console.log(myPersevereCodeObj)
+
+// Now add a new property called `cumulativeScore` and set it to 50.
+myPersevereCodeObj.cumulativeScore = 50;
+console.log(myPersevereCodeObj)
+
+// Now add your `currentScore` and your `cumulativeScore` and put it back in `cumulativeScore`.
+myPersevereCodeObj.cumulativeScore = myPersevereCodeObj.currentScore + myPersevereCodeObj.cumulativeScore;
+console.log(myPersevereCodeObj)
+
+// Now add a property called `I am exactly this cool` (you'll need bracket notation!) and set it to `soooooo cool`.
+myPersevereCodeObj["I am exactly this cool"] = "soooooo cool";
+console.log(myPersevereCodeObj)
+
+// Part 2 - your own object.
+// Now make a new EMPTY object that represents yourself. You won't put any properties in the curly braces, but just like in part 1, add them in each step below using plain JavaScript statements.
+const myOwnObject = {
+
+}
+console.log(myOwnObject)
+// Add a `firstName` property and set it to a string.
+myOwnObject.firstName = "Jesse"
+console.log(myOwnObject)
+
+// Add a `lastName` property and set it to a string.
+myOwnObject.lastName = "Davis"
+console.log(myOwnObject)
+
+// Add a `fullName` property and set it to the name properties put together with a space string in the middle.
+myOwnObject.fullName = myOwnObject.firstName +  " "  + myOwnObject.lastName;
+console.log(myOwnObject)
+
+// Add an `age` property and set it to a number.
+myOwnObject.age = 38;
+console.log(myOwnObject)
+
+
+// Add an address property and set it to an empty array.
+myOwnObject.address = [];
+console.log(myOwnObject)
+
+// Now push three strings into that array.
+myOwnObject["address"] = ["9675", "Sparta", "Hwy"];
+console.log(myOwnObject)
+
+
+
+
+// Part 3 - setting key-value pairs in-line.
+// Now make a new object, but this time we'll pre-populate it, not set it in any lines below it. In other words, the properties should be set the same way I initially set the `myPersevereCodeObj` on line 1. It can be called whatever you want and can have whatever properties you want, as long as they include at least one string value, boolean value, number value, and array of something. The important thing here is: this should all be done within the curly brackets of the object, not below the place where the object is declared.
+
+
+
+
+// As a stretch goal, go back pre-populate the object with at least one property using a key name that CAN'T be a variable name. That would be anything with a space in it, or a dash, or starting with a number.
